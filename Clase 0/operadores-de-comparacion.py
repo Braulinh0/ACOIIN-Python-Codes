b1171f0b62dcac48a10e509f0fa4004821e59128
Braulio Peña <xxbraulioninjayt133@gmail.com>
2025-06-06 21:44:21 -0400

    Create operadores-de-comparacion.py

---
+++ Clase 0/operadores-de-comparacion.py
@@ -0,0 +1,12 @@
+# Operadores de comparación
+print("- Operadores de comparacion\n")
+
+mayor_que = 5 > 3
+menor_que = 5 < 3
+igual_a = 5 == 3
+diferente_de = 5 != 3
+
+print("Si la condicion es verdadera, entrega:", mayor_que)
+print("Si la condicion es falsa, entrega:", menor_que)
+print("Si la condicion es falsa, entrega:", igual_a)
+print("Si la condicion es verdadera, entrega:", diferente_de, "\n")
