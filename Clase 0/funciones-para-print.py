e60f77383e5fee19e1ff37987bdf7558a101c730
Braulio Peña <xxbraulioninjayt133@gmail.com>
2025-06-06 21:40:09 -0400

    Create funciones-para-print.py

---
+++ Clase 0/funciones-para-print.py
@@ -0,0 +1,15 @@
+# Función para imprimir (output)
+print("- Funciones para imprimir\n")
+print("1. Hello, world\n") # Imprime Hello, world por pantalla
+
+print("2. Hello", "world") # Imprime Hello world. La coma (,) es equivalente a un espacio
+print("3. Hello", "ACOI", "IN\n") # Imprime Hello ACOI IN. La coma (,) es equivalente a un espacio
+
+print("4. Línea 1\n4. Línea 2\n") # \n permite hacer un salto de línea o un enter
+
+print("5. Soy un slash\\") # \\ permite imprimir solo un \
+print("6. Soy dos slashes\\\\\n") # Se imprime \\, básicamente: \\ = \
+    
+print("7. Soy una doble comilla\n") # \" permite imprimir solo una comilla doble (")
+
+print("8. Soy una\tTabulacion\n") # \t realiza una tabulación equivalente a 6 espacios
