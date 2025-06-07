f4a8002608a53b1679124428d7448193c6c36983
Braulio Peña <xxbraulioninjayt133@gmail.com>
2025-06-06 21:41:04 -0400

    Create variables-tipos-de-datos.py

---
+++ Clase 0/variables-tipos-de-datos.py
@@ -0,0 +1,14 @@
+# Variables y tipos de datos
+print("- Variables y tipos de datos\n")
+
+entero = 10 # Variable int
+flotante = 10.5 # Variable float
+cadena = "ACOIIN es goooooood" # Variable str
+booleano_true = True # Variable bool (Verdadera)
+booleano_false = False # Variable bool (Falsa)
+
+print("1. Valor de entero:", entero, "Tipo:", type(entero)) # Imprime la clase de la variable
+print("2. Valor de flotante:", flotante, "Tipo:", type(flotante)) # Imprime la clase de la variable
+print("3. Valor de cadena:", cadena, "Tipo:", type(cadena)) # Imprime la clase de la variable
+print("4. Valor de verdadero:", booleano_true, "Tipo:", type(booleano_true)) # Imprime la clase de la variable (True)
+print("5. Valor de falso:", booleano_false, "Tipo:", type(booleano_false), "\n")  # Imprime la clase de la variable (False)
