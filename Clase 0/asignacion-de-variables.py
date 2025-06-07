6e24169de2cf4a98be100ec1b761b34caba6d923
Braulio Peña <xxbraulioninjayt133@gmail.com>
2025-06-06 21:42:07 -0400

    Create asignacion-de-variables.py

---
+++ Clase 0/asignacion-de-variables.py
@@ -0,0 +1,13 @@
+# Asignación de variables
+print("- Asignación de variables\n")
+variable_1 = 0
+variable_2 = 10
+
+print("1. Valor de la variable 1:", variable_1)
+print("1. Valor de la variable 2:", variable_2, "\n")
+
+print("Actualizamos variable_1 <- variable_2...\n")
+variable_1 = variable_2
+
+print("2. Valor de la variable 1:", variable_1,)
+print("2. Valor de la variable 2:", variable_2, "\n")
