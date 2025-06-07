1f946469895bffa5bee6d42acf3dd6394279b017
Braulio Peña <xxbraulioninjayt133@gmail.com>
2025-06-06 21:43:53 -0400

    Create operadores-basicos.py

---
+++ Clase 0/operadores-basicos.py
@@ -0,0 +1,18 @@
+# Operadores básicos
+print("- Operadores básicos\n")
+
+suma = 5 + 3
+resta = 5 - 3
+multiplicacion = 5 * 3
+division = 5 / 3
+potencia = 5 ** 3
+modulo = 5 % 3
+division_entera = 5 // 3
+
+print("1. La suma de 5 + 3 =", suma)
+print("2. La resta de 5 - 3 =", resta)
+print("3. La multiplicación de 5 * 3 =", multiplicacion)
+print("4. La división de 5 / 3 =", division)
+print("5. La potencia de 5 ^ 3 =", potencia)
+print("6. La división entera de 5 // 3 =", division_entera)
+print("7. El módulo de 5 % 3 =", modulo, "\n")
